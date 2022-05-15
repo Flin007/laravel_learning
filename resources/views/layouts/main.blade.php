302083206e45b3a175a0a5c59567397246f44fe7
Vladimir <Flin010@inbox.ru>
2022-05-16 00:48:39 +0300

    bootstap and little style for posts page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,17 +5,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <ul>
-        <li><a href="{{ route('index.index') }}">Главная</a></li>
-        <li><a href="{{ route('post.index') }}">Посты</a></li>
-        <li><a href="{{ route('contact.index') }}">Контакты</a></li>
-        <li><a href="{{ route('about.index') }}">О нас</a></li>
-    </ul>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+            <span class="fs-4">First Laravel project</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link {{ Request::routeIs('index.index') ? 'active' : '' }}" href="{{ route('index.index') }}">Главная</a></li>
+            <li class="nav-item"><a class="nav-link {{ Request::routeIs('post.index') ? 'active' : '' }}" href="{{ route('post.index') }}">Посты</a></li>
+            <li class="nav-item"><a class="nav-link {{ Request::routeIs('contact.index') ? 'active' : '' }}" href="{{ route('contact.index') }}">Контакты</a></li>
+            <li class="nav-item"><a class="nav-link {{ Request::routeIs('about.index') ? 'active' : '' }}" href="{{ route('about.index') }}">О нас</a></li>
+        </ul>
+    </header>
 </div>
-    @yield('content')
+<div class="container">
+    <div class="row">
+        @yield('content')
+    </div>
+</div>
 </body>
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.main')
 @section('content')
     <h1>This is posts page</h1>
-    @foreach($posts as $post)
-        <div>{{ $post->title }}</div>
-        <div>{{ $post->content }}</div>
-        <div>{{ $post->likes }}</div>
-    @endforeach
+    <div class="posts row px-3">
+        @foreach($posts as $post)
+            <a href="/posts/{{ $post->id }}" class="card col-md-3 gx-3" style="text-decoration: unset; color: unsetgit">
+                <img src="{{ $post->image }}" class="card-img-top" alt="...">
+                <div class="card-body" style="text-decoration: none">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">{{ $post->content }}</p>
+                    <span href="#" class="btn btn-primary">{{ $post->likes }}</span>
+                </div>
+            </a>
+        @endforeach
+    </div>
 @endsection
