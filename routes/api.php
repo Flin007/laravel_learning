345e673cfea5423cffa33c3b347dad99850fb54f
Vladimir <Flin010@inbox.ru>
2022-05-29 03:30:19 +0300

    JWT toker, API через авторизацию JWT

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+Route::group([
+    'namespace' => 'Posts',
+    'middleware' => 'jwt.auth'
+], function(){
+    Route::get('/posts', 'IndexController');
+});
